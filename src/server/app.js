b0d9a0077b67c1eaa90e30371587260a3d09061b
WillMW02 <williammw.wmw@gmail.com>
2021-12-18 19:43:05 +0000

    feat: add .env support for some variables such as the webserver port

--- src/server/app.js
+++ src/server/app.js
@@ -1,3 +1,4 @@
+import './env.config.js'; // workaround for ES imports being hoisted, to import the env file before other modules
 import express from 'express';
 import { join, dirname} from 'path';
 import { fileURLToPath } from 'url';
@@ -8,6 +9,8 @@ const __dirname = fileURLToPath(dirname(import.meta.url)); // workaround for ES6
 
 app.use(express.static(join(__dirname, '../public')));
 
-app.listen(process.env.PORT??80, () => {
-	logger.info(`Listening on port ${process.env.PORT??80}`);
+app.listen(process.env.PORT??8080, () => {
+	logger.info(`Listening on port ${process.env.PORT??8080}`);
 });
+
+export default app
