f4c9ae7649a71095b8a15fe35facf1a1fc7737a6
WillMW02 <williammw.wmw@gmail.com>
2021-12-18 04:50:30 +0000

    chore: add basic ESLint configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,41 @@
+module.exports = {
+	'env': {
+		'node': true,
+		'es2021': true
+	},
+	'extends': 'eslint:recommended',
+	'parserOptions': {
+		'ecmaVersion': 13,
+		'sourceType': 'module'
+	},
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'windows'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		],
+		'require-jsdoc' : [
+			'error',
+			{
+				'require': {
+					'FunctionDeclaration': true,
+					'MethodDefinition': true,
+					'ClassDeclaration': false,
+					'ArrowFunctionExpression': true,
+					'FunctionExpression': true
+				}
+			}
+		]
+	}
+};
