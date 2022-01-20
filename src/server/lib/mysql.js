b4f75094595a030d7693bc6491ac0cd1c13a32a1
WillMW02 <williammw.wmw@gmail.com>
2022-01-20 20:09:19 +0000

    feat: add database pool functionality

---
+++ src/server/lib/mysql.js
@@ -0,0 +1,15 @@
+import { createPool } from 'mariadb';
+
+const conn_pool = createPool({
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	database: process.env.DB_NAME
+});
+
+/**
+ * Get a connection to the database from the connection pool.
+ * @returns {Promise<PoolConnection>}
+ */
+export const getConn = () => conn_pool.getConnection();
