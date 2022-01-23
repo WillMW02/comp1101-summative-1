a34fcb2652aec79423061ec944b4644fb9c53fd0
WillMW02 <williammw.wmw@gmail.com>
2022-01-23 15:59:30 +0000

    docs: add API documentation to review route

--- src/server/routes/review.route.js
+++ src/server/routes/review.route.js
@@ -3,12 +3,153 @@ import * as ReviewController from '../controllers/review.controller.js';
 
 const router = Router();
 
+/**
+ * @swagger
+ * /review:
+ *   get:
+ *     summary: Retrieve all reviews
+ *     tags: 
+ *       - reviews
+ *     responses:
+ *       200:
+ *         description: A list of all reviews
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Review'
+ */
 router.get('/', ReviewController.getReviews);
+
+/**
+ * @swagger
+ * /review/{id}:
+ *   get:
+ *     summary: Retrieve a single review specified by ID
+ *     tags: 
+ *       - reviews
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The Unique Identifier of the review to be retrieved
+ *         schema:
+ *           type: integer
+ *           example: 0
+ *     responses:
+ *       200:
+ *         description: A single review
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Review'
+ *       404:
+ *         description: No review matching the provided ID could be found
+ */
 router.get('/:id', ReviewController.getReview);
 
+/**
+ * @swagger
+ * /review:
+ *   post:
+ *     summary: Create a review
+ *     tags: 
+ *       - reviews
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - in: body
+ *         name: review
+ *         description: The review to be created
+ *         schema:
+ *           type: object
+ *           required:
+ *             - user_id
+ *             - title
+ *             - content
+ *             - rating
+ *           properties:
+ *             user_id:
+ *               type: integer
+ *               description: The unique identifier of the user posting the review
+ *               example: 10
+ *             title:
+ *               type: string
+ *               description: The title of the review to be created
+ *               example: Mediocre Service
+ *             content:
+ *               type: string
+ *               description: The description of the review
+ *               example: Experience was sub par, nothing special.
+ *             rating:
+ *               type: integer
+ *               minimum: 0
+ *               maximum: 5
+ *               description: the rating (in stars) between one and five
+ *               example: 3
+ *     responses:
+ *       201:
+ *         description: Review Created
+ */
 router.post('/', ReviewController.createReview);
 
+/**
+ * @swagger
+ * /review/{id}:
+ *   delete:
+ *     summary: Delete a single review specified by ID
+ *     tags: 
+ *       - reviews
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The Unique Identifier of the review to be deleted
+ *         schema:
+ *           type: integer
+ *           example: 0
+ *     responses:
+ *       202:
+ *         description: Successful deletion
+ *               
+ *       404:
+ *         description: Review not found
+ */
 router.delete('/:id', ReviewController.deleteReview);
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Review:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: A unique identifier for the review
+ *           example: 9 
+ *         title:
+ *           type: string
+ *           description: The title of the review
+ *           example: Great Experience
+ *         content:
+ *           type: string
+ *           description: The review content
+ *           example: Friendly staff, overall good experience
+ *         date:
+ *           type: string
+ *           format: date-time
+ *           description: Date of review being posted
+ *         name:
+ *           type: string
+ *           description: The name of the reviewer
+ *           example: William Maltby-Wehner
+ *         avatar:
+ *           type: string
+ *           description: The B64 encoded avatar of the user
+ *           example: QmlnIHVwIHN0ZXZlbiBicmFkbGV5
+ */
 
 export default router;
 
