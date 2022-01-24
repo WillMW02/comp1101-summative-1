75775719a30a5e34bc3a338841dd8588389f954c
WillMW02 <williammw.wmw@gmail.com>
2022-01-24 02:01:36 +0000

    tests: add jest configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,8 @@ module.exports = {
 	'env': {
 		'node': true,
 		'es2021': true,
-		'browser': true
+		'browser': true,
+		'jest': true
 	},
 	'extends': 'eslint:recommended',
 	'parserOptions': {
---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+	collectCoverage: true,
+	coverageDirectory: 'src/coverage',
+	coverageProvider: 'v8',
+	testEnvironment: 'node',
+};
