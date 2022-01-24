4577b4bda4962cd49c4a410ffb981ba3afecf77d
WillMW02 <williammw.wmw@gmail.com>
2022-01-24 19:06:51 +0000

    feat: add user API functionality

--- src/server/controllers/user.controller.js
+++ src/server/controllers/user.controller.js
@@ -1,46 +1,129 @@
 /* eslint-disable no-unused-vars */
+import logger from '../lib/logger.js';
 import * as UserModel from '../models/user.model.js';
 
 /**
  * 
- * @param {*} req 
- * @param {*} res 
+ * @param {Express.Request} req 
+ * @param {Express.Response} res 
+ * @param {function} next
  * @returns void
  */
-export const getUsers = (req, res) => {
-	res.status(501);
-	res.send();
+export const getUsers = async (req, res, next) => {
+	let dat;
+	try {
+		dat = await UserModel.getAll();
+		if(dat) res.json(dat);
+		res.status(404);
+		res.send();
+	} catch(err) {
+		res.status(500);
+		res.send();
+		next(err);
+		return;
+	}
 };
 
 /**
  * 
- * @param {*} req 
- * @param {*} res 
+ * @param {Express.Request} req 
+ * @param {Express.Response} res 
+ * @param {function} next
  * @returns void
  */
-export const getUser = (req, res) => {
-	res.status(501);
-	res.send();
+export const getUser = async (req, res, next) => {
+	let dat;
+	try {
+		dat = await UserModel.get(req.params.id);
+		if(dat) res.json(dat);
+		res.status(404);
+		res.send();
+	} catch(err) {
+		res.status(500);
+		res.send();
+		next(err);
+		return;
+	}
 };
 
 /**
  * 
- * @param {*} req 
- * @param {*} res 
+ * @param {Express.Request} req 
+ * @param {Express.Response} res 
+ * @param {function} next
  * @returns void
  */
-export const createUser = (req, res) => {
-	res.status(501);
-	res.send();
+export const getUserByName = async (req, res, next) => {
+	let dat;
+	try {
+		dat = await UserModel.getByName(req.params.name);
+		if(dat) res.json(dat);
+		res.status(404);
+		res.send();
+	} catch(err) {
+		res.status(500);
+		res.send();
+		next(err);
+		return;
+	}
+};
+
+
+/**
+ * 
+ * @param {Express.Request} req 
+ * @param {Express.Response} res 
+ * @param {function} next
+ * @returns void
+ */
+export const createUser = async (req, res, next) => {
+	logger.info(`createUser Invoked with ${JSON.stringify(req.body)}`, true);
+
+	if(!(req.body && req.body.name && req.body.avatar)) {
+		res.status(406);
+		return res.json({
+			err: 'Request body did not include required parameters'
+		});
+	}
+	let dat;
+	try {
+		dat = await UserModel.create(req.body);
+		res.status(201);
+		if(dat) {
+			if(dat.err) {
+				res.status(409);
+			}
+			return res.json(dat);
+		}
+		res.status(500);
+		return res.send();
+	} catch(err) {
+		res.status(500);
+		res.send();
+		next(err);
+		return;
+	}
 };
 
 /**
  * 
- * @param {*} req 
- * @param {*} res 
+ * @param {Express.Request} req 
+ * @param {Express.Response} res 
+ * @param {function} next
  * @returns void
  */
-export const deleteUser = (req, res) => {
-	res.status(501);
-	res.send();
+export const deleteUser = async (req, res, next) => {
+	try {
+		if(await UserModel.remove(req.params.id)) {
+			res.status(202);
+		} else {
+			res.status(404);
+		}
+		res.send();
+	} catch(err) {
+		res.status(500);
+		res.send();
+		next(err);
+		return;
+	}
 };
---
+++ src/server/models/user.model.js
@@ -0,0 +1,116 @@
+import { getConn } from '../lib/mysql.js';
+import SqlCommands from '../config/sqlCommands.config.json';
+import logger from '../lib/logger.js';
+
+/**
+ * Get a user by id
+ * 
+ * @param {string | number} id the ID of the user to be retrieved
+ * 
+ * @returns {Object} Object containing the user.
+ */
+export const get = async id => {
+	const conn = await getConn();
+	try {
+		const rows = await conn.query(SqlCommands.users.get, [id]);
+		return rows[0];
+	} catch(err) {
+		logger.error(err, true);
+		throw new Error('An error occured whilst fetching a user by id');
+	} finally {
+		if (conn) conn.end();
+	}
+};
+
+/**
+ * Get a user by the name column
+ * 
+ * @param {string} name
+ * 
+ * @returns {Object} Object containing the user.
+ */
+export const getByName = async name => {
+	const conn = await getConn();
+	try {
+		const rows = await conn.query(SqlCommands.users.getByName, [name]);
+		return rows[0];
+	} catch(err) {
+		logger.error(err, true);
+		throw new Error('An error occured whilst fetching a user by username');
+	} finally {
+		if (conn) conn.end();
+	}
+};
+
+/**
+ * Get all users in the database
+ * 
+ * @returns {Object[]} Array containing all users
+ */
+export const getAll = async () => {
+	const conn = await getConn();
+	try {
+		const rows = await conn.query(SqlCommands.users.getAll);
+		return rows;
+	} catch(err) {
+		logger.error(err, true);
+		throw new Error('An error occured whilst fetching users');
+	} finally {
+		if (conn) conn.end();
+	}
+};
+
+/**
+ * Create a users from provided object
+ * 
+ * @param {Object} user
+ * @param {string} user.name
+ * @param {string} user.avatar
+ * 
+ * @returns {Object} Object contining the ID of the created user 
+ */
+export const create = async user => {
+	logger.info(`UserModel.create invoked with ${JSON.stringify(user)}`, true);
+	const conn = await getConn();
+	try {
+		let rows = await conn.query(SqlCommands.users.existsByName, [user.name]);
+		if(rows.length == 0) {
+			rows = await conn.query(SqlCommands.users.create, [user.name, user.avatar]);
+			return {
+				id: rows[0].insertId
+			};
+		} return {
+			err: 'User Already Exists'
+		};
+		
+	} catch(err) {
+		logger.error(err, true);
+		throw new Error('An error occured whilst creating a user');
+	} finally {
+		if (conn) conn.end();
+	}
+};
+
+/**
+ * Delete a user from the database
+ * 
+ * @param {string | number} id the ID of the user to be deleted
+ * 
+ * @returns {boolean} Whether the deletion was successful
+ */
+export const remove = async id => {
+	const conn = await getConn();
+	try {
+		let rows = await conn.query(SqlCommands.users.exists, [id]);
+		if(rows.length > 0) {
+			rows = await conn.query(SqlCommands.users.delete, [id]);
+			return true;
+		} 
+		return false;
+	} catch(err) {
+		logger.error(err, true);
+		throw new Error('An error occured whilst deleting a user');
+	} finally {
+		if (conn) conn.end();
+	}
+};
