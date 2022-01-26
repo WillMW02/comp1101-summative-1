02ff2c71663bb6f7461e5f4131867caabc463deb
WillMW02 <williammw.wmw@gmail.com>
2022-01-26 00:42:47 +0000

    feat: tidy up clientside js, removing uneccesary class

--- src/public/assets/js/review.js
+++ src/public/assets/js/review.js
@@ -1,5 +1,3 @@
-import { ElementBuilder } from './elementBuilder.js';
-
 export class Review {
 	/**
 	 * @param {Object} review
@@ -17,5 +15,43 @@ export class Review {
 
 	stringify = () => JSON.stringify(this);
 
-	generateElement = () => ElementBuilder.buildReview(this);
+	generateElement = () => {
+		let instance = `
+		<div class="card review">
+			<div class="card-content">
+				<div class="content">
+					<p class="title is-3">{{Review_Title}}</p>
+					{{Review_Content}}
+				</div>
+				<div class="media is-left">
+					<div class="media-left">
+					<figure class="image is-48x48">
+						<img src="{{Author_Avatar}}" alt="user avatar">
+					</figure>
+					</div>
+					<div class="media-content">
+						<p><span class="title is-5">{{Author_Name}}</span> <span class="subtitle is-7">{{Review_Time}}</span></p>
+						<p class="subtitle is-6">
+							{{Review_Stars}}
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>`;
+		instance = instance.replaceAll('{{Review_Title}}', this.title);
+		instance = instance.replaceAll('{{Review_Content}}', this.content);
+		instance = instance.replaceAll('{{Review_Time}}', this.date);
+		instance = instance.replaceAll('{{Author_Avatar}}', this.avatar);
+		instance = instance.replaceAll('{{Author_Name}}', this.name);
+
+		let stars = '';
+		for(let i=0; i<5; i++) stars += `
+			<span class="icon star {{Active}}">
+				<i class="fas fa-star"></i>
+			</span>`.replaceAll('{{Active}}', i<this.rating?'active':'');
+
+		instance = instance.replaceAll('{{Review_Stars}}', stars);
+
+		return instance;
+	};
 }
